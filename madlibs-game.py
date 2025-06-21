75ba55b6623c5448fe3d59fabdb40043ba963691
Hasan Mrisat <hasanmri890@gmail.com>
2025-06-21 04:20:03 +0300

    Add files via upload

---
+++ madlibs-game.py
@@ -0,0 +1,14 @@
+## MADLIBS GAME - U ENTER RANDOM WORDS TO CREATE A STORY ##
+ 
+ 
+adjective1 = input("enter an adjective :")
+noun1 = input("enter an noun :")
+adjective2 = input("enter an adjective :")
+verb1 = input("enter an verb + ing :")
+adjective3 = input("enter an adjective :")
+
+
+print(f"today i went to a {adjective1} zoo.")
+print(f"in an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I Was {adjective3}!") 
